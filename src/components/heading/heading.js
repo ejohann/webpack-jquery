a6079ea4b9339f4f85f0bb8c2c5d11535fd7e197
Johanne <johanne_lewis@hotmail.com>
2019-08-23 11:10:13 +0100

    Convert vanilla javascript to jquery

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -1,11 +1,12 @@
 import './heading.scss';
+import $ from 'jquery';
 
 class Heading{
     render(){
-        const h1 = document.createElement('h1');
-        const body = document.querySelector('body');
-        h1.innerHTML = 'Webpack is awesome';
-        body.appendChild(h1);
+        const h1 = $('<h1>');
+        const body = $('body');
+        h1.text('Webpack is awesome');
+        body.append(h1);
     }
 }
 
